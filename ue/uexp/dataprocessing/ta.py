3b8cc0579fc26a7726a822a98c85979042aa8e3b
Dan Yang <56417240+ckive@users.noreply.github.com>
2022-02-05 18:12:04 -0600

    completed dense mlp

---
+++ ue/uexp/dataprocessing/ta.py
@@ -0,0 +1,43 @@
+def computeRSI (data, time_window):
+    diff = data.diff(1).dropna()        # diff in one field(one day)
+
+    #this preservers dimensions off diff values
+    up_chg = 0 * diff
+    down_chg = 0 * diff
+    
+    # up change is equal to the positive difference, otherwise equal to zero
+    up_chg[diff > 0] = diff[ diff>0 ]
+    
+    # down change is equal to negative deifference, otherwise equal to zero
+    down_chg[diff < 0] = diff[ diff < 0 ]
+    
+    # check pandas documentation for ewm
+    # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.ewm.html
+    # values are related to exponential decay
+    # we set com=time_window-1 so we get decay alpha=1/time_window
+    up_chg_avg   = up_chg.ewm(com=time_window-1 , min_periods=time_window).mean()
+    down_chg_avg = down_chg.ewm(com=time_window-1 , min_periods=time_window).mean()
+    
+    rs = abs(up_chg_avg/down_chg_avg)
+    rsi = 100 - 100/(1+rs)
+    return rsi
+
+
+def sRSI(data, k_window=3, d_window=3, window=14):
+    #https://www.cmcmarkets.com/en/trading-guides/what-is-a-stochastic-indicator
+    
+    # input to function is one column from df
+    # containing closing price or whatever value we want to extract K and D from
+    
+    min_val  = data.rolling(window=window, center=False).min()
+    max_val = data.rolling(window=window, center=False).max()
+    
+    stoch = ( (data - min_val) / (max_val - min_val) ) * 100
+    
+    K = stoch.rolling(window=k_window, center=False).mean() 
+    #K = stoch
+    
+    D = K.rolling(window=d_window, center=False).mean() 
+
+
+    return K, D
